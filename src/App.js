7c9df7830a1351699b23d7ad171fd431a7ece250
Icika-max <114281964+Icika-max@users.noreply.github.com>
2023-01-16 15:27:21 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, {useEffect,useState} from 'react';
+import Transactions from './components/Transactions';
+import NewItemForm from './components/NewItemForm';
+import SearchForm from './components/SearchForm';
+
+
+function App() {
+  const[transactions, setTransactions]= useState([])
+useEffect(()=>{
+  fetch("http://localhost:6001/transactions")
+  .then(r=>r.json())
+  .then(transc=>setTransactions(transc))
+},[])
+console.log(transactions)
+function handleUpdateOnSubmission(newTransaction){
+  console.log(newTransaction)
+  setTransactions(transactions=>[...transactions,newTransaction])
+  const serverOption={
+    method:"POST",
+    headers:{
+      "content-type": "application/json"
+    },
+    body:JSON.stringify(newTransaction)
+  }
+fetch("http://localhost:6001/transactions",serverOption)
+.then(r=>r.json())
+.then(newItem=>console.log(newItem))
+}
+  return (
+    <div className="ui raise segment">
+      <div className='header-text'>
+        <h2>The Bank of Flatiron</h2>                
+      </div>
+      <SearchForm/>
+      <NewItemForm onSubmission={handleUpdateOnSubmission}/>
+        <Transactions transactions={transactions}/>
+       
+
+    </div>
+  );
+}
+
+export default App;
